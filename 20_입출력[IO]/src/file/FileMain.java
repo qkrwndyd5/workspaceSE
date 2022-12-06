16b00a475b89e1140871f7096d0784254a498df0
qkrwndyd5 <pjyy9607@naver.com>
2022-12-06 13:43:23 +0900

    .

---
+++ 20_입출력[IO]/src/file/FileMain.java
@@ -0,0 +1,23 @@
+package file;
+
+import java.io.File;
+
+public class FileMain {
+
+	public static void main(String[] args) {
+		/*
+		 * 현재파일경로[기준경로]
+		 *    eclipse --> C:\2022-11-java-developer\workspaceSE\20_입출력[IO]
+		 *    
+		 * 상대경로(relative)경로
+		 *    현재경로를 기준으로 경로를기술
+		 *    
+		 * 절대경로(absolute)경로   
+		 *    C:,D: 드라이브를 기준으로 경로기술
+		 */
+		
+		System.out.println(File.separatorChar);
+
+	}
+
+}
